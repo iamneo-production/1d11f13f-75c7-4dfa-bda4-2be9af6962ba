d43201f7c8affdd00b12a1db15a3abb6fd41b0a5
Manikandan004 <maniprasanth12@gmail.com>
2023-07-21 07:27:45 +0000

    push to my branch

--- springapp/src/test/java/com/admissionportal/adminportal/AdminportalApplicationTests.java
+++ springapp/src/test/java/com/admissionportal/adminportal/AdminportalApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springapp;
+package com.admissionportal.adminportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringappApplicationTests {
+class AdminportalApplicationTests {
 
 	@Test
 	void contextLoads() {
