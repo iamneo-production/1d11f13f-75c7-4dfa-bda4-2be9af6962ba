11d254d32ac7cff68d5551746b83fb93aed18c74
THARANISOMASUNDARAM <tharanipw2001@gmail.com>
2023-07-25 10:17:24 +0000

    commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdmissionComponent } from './admission/admission.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import { EnrolledCourseComponent } from './enrolled-course/enrolled-course.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { AddStudentComponent } from './add-student/add-student.component';
 import { UpdateStudentComponent } from './update-student/update-student.component';
@@ -15,9 +12,6 @@ import { UpdateStudentComponent } from './update-student/update-student.componen
 @NgModule({
   declarations: [
     AppComponent,
-    AdmissionComponent,
-    HeaderComponent,
-    EnrolledCourseComponent,
     StudentListComponent,
     AddStudentComponent,
     UpdateStudentComponent,
